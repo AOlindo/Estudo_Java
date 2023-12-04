8d9d214dc6a15636cc068a5e6ff341328bff40bd
Adriana Olindo <adriana-olindo18@hotmail.com>
2023-12-04 16:58:39 -0300

    Implementando a comunicação com o Web Service

--- desafio/src/module-info.java
+++ desafio/src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module desafio {
+	requires java.net.http;
 }
--- desafio/src/screenmatch/PrincipalComBusca.java
+++ desafio/src/screenmatch/PrincipalComBusca.java
@@ -1,5 +1,27 @@
 package screenmatch;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
 public class PrincipalComBusca {
 
+	public static void main(String[] args) throws IOException, InterruptedException {
+		Scanner leitura = new Scanner(System.in);
+		System.out.println("Digite um filme para busca: ");
+		var busca = leitura.nextLine();
+
+		String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=" + System.getenv("apikey");
+
+		//Essas informações tem na documentação JavaDoc https://docs.oracle.com/en/java/javase/17/docs/api/java.net.http/java/net/http/HttpRequest.html
+		//Requisição
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
+		//Resposta
+		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+		System.out.println(response.body());
+	}
 }
