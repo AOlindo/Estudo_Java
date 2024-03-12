92862b4a2267f8476a71f749f47c56bc4c70e24b
Adriana Olindo <adriana-olindo18@hotmail.com>
2024-03-12 16:01:12 -0300

    Projeto estoque livraria criado

---
+++ estoque-livraria/src/test/java/br/com/olindo/estoquelivraria/EstoqueLivrariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.olindo.estoquelivraria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueLivrariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
